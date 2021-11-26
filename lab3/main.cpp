0386670c4b5fc870f9aeb87877ef89e72688b01e
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2021-11-26 17:14:58 +0700

    add lab3

---
+++ lab3/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <fstream>
+#include "WorkflowExecutor.h"
+
+int main() {
+    std::ifstream file;
+    file.open("D:/cpp/cpp_nsu_labs/lab3/in.txt");
+    WorkflowExecutor executor;
+    executor.ExecuteWorkflow(file);
+    file.close();
+    return 0;
+}
