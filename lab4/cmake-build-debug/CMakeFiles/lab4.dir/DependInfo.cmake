f483cd30d2d8c813c7362882b02fdf330a2c1df6
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2021-12-17 21:03:03 +0700

    add main

---
+++ lab4/cmake-build-debug/CMakeFiles/lab4.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/cpp/cpp_nsu_labs/lab4/Controller.cpp" "D:/cpp/cpp_nsu_labs/lab4/cmake-build-debug/CMakeFiles/lab4.dir/Controller.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab4/Model.cpp" "D:/cpp/cpp_nsu_labs/lab4/cmake-build-debug/CMakeFiles/lab4.dir/Model.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab4/View.cpp" "D:/cpp/cpp_nsu_labs/lab4/cmake-build-debug/CMakeFiles/lab4.dir/View.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab4/main.cpp" "D:/cpp/cpp_nsu_labs/lab4/cmake-build-debug/CMakeFiles/lab4.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lab4/cmake-build-debug/CMakeFiles/lab4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4.dir/Controller.cpp.obj"
+  "CMakeFiles/lab4.dir/Model.cpp.obj"
+  "CMakeFiles/lab4.dir/View.cpp.obj"
+  "CMakeFiles/lab4.dir/main.cpp.obj"
+  "lab4.exe"
+  "lab4.exe.manifest"
+  "lab4.pdb"
+  "liblab4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
