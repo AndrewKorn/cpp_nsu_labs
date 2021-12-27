f17bf84111f3a0c11a6aceadb6284797d82e8601
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2021-12-27 23:16:10 +0700

    rename

--- lab4(MVC Bulls&Cows)/cmake-build-debug/cmake_install.cmake
+++ lab4(MVC Bulls&Cows)/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/cpp/cpp_nsu_labs/lab4
+# Install script for directory: D:/cpp/cpp_nsu_labs/lab4(MVC Bulls&Cows)
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/cpp/cpp_nsu_labs/lab4/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/cpp/cpp_nsu_labs/lab4(MVC Bulls&Cows)/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
