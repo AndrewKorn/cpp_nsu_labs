3f057e3cff3c82a5724f194d698a1b0f4efecf0c
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2021-12-27 23:23:32 +0700

    rename

---
+++ lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/Blocks/Dump.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/Blocks/Dump.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/Blocks/Grep.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/Blocks/Grep.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/Blocks/ReadFile.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/Blocks/ReadFile.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/Blocks/Replace.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/Blocks/Replace.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/Blocks/Sort.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/Blocks/Sort.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/Blocks/WriteFile.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/Blocks/WriteFile.cpp.obj"
+  "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/main.cpp" "D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/cmake-build-debug/CMakeFiles/lab3.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
