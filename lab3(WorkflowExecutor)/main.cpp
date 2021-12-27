44038a24f19ed6f621846539461c6638fc20e516
AndrewKorn <a.korneshchuk1@g.nsu.ru>
2021-12-27 23:22:21 +0700

    rename

--- lab3(WorkflowExecutor)/main.cpp
+++ lab3(WorkflowExecutor)/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
     std::ifstream file;
-    file.open("D:/cpp/cpp_nsu_labs/lab3/in1.txt");
+    file.open("D:/cpp/cpp_nsu_labs/lab3(WorkflowExecutor)/in1.txt");
     WorkflowExecutor executor;
     executor.ExecuteWorkflow(file);
     file.close();
